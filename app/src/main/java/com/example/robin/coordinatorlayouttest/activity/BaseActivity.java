2070d4c5cc10ad0f352ace30e864d13bbc85d648
guaren2004 <guaren2005@126.com>
2017-01-30 11:00:30 +0800

    add a TAG in BaseActivity

--- app/src/main/java/com/example/robin/coordinatorlayouttest/activity/BaseActivity.java
+++ app/src/main/java/com/example/robin/coordinatorlayouttest/activity/BaseActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 public class BaseActivity extends AppCompatActivity {
 
     protected Context context;
+    protected final String TAG = this.getClass().getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
